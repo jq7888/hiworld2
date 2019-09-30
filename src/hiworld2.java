01cabc4596857d56aa415db1ee971cf1e65de7fb
jq7888 <2058878376@qq.com>
2019-09-30 16:09:29 +0800

    222

---
+++ src/hiworld2.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName:hiworld2
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ * @Date:2019/9/30 15:59
+ * @Author:jiaqi@163.com
+ */
+public class hiworld2 {
+    public static void main(String[] args) {
+        System.out.println("222");
+    }
+}
